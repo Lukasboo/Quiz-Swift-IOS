038f347cf002ed0a83bab10bb1f03e60f0194dde
Ricardo Corrales <ricardocorrales@Mac-mini-de-Ricardo.local>
2017-12-12 18:48:28 -0200

    First Commit

---
+++ SwiftQuiz/ResultViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ResultViewController.swift
+//  SwiftQuiz
+//
+//  Created by Ricardo Corrales on 12/12/17.
+//  Copyright © 2017 Ricardo Corrales. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var lbAnswered: UILabel!
+    @IBOutlet weak var lbCorrect: UILabel!
+    @IBOutlet weak var lbWrong: UILabel!
+    @IBOutlet weak var lbScore: UILabel!
+    
+    var totalCorrectAnswers: Int = 0
+    var totalAnswers: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        lbAnswered.text = "Perguntas Respondidas: \(totalAnswers)"
+        lbCorrect.text = "Perguntas corretas: \(totalCorrectAnswers)"
+        lbWrong.text = "Perguntas erradas: \(totalAnswers - totalCorrectAnswers)"
+        
+        let score = totalCorrectAnswers*100/totalAnswers
+        lbScore.text = "\(score)%"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func close(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
